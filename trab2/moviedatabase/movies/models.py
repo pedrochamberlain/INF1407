620b16f688c40f7c6eb533e82694fb1f1e85f989
Pedro Chamberlain <pedrochmatos@gmail.com>
2021-11-18 00:56:28 -0300

    use movie number id as primary key

--- trab2/moviedatabase/movies/models.py
+++ trab2/moviedatabase/movies/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Filme(models.Model):
-
+    id = models.AutoField(primary_key=True)
     title = models.CharField(
         max_length=100, verbose_name='Título do filme')
     year_of_release = models.IntegerField(
